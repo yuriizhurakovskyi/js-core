83ad71ceed01ced10b934eb380315fa3f08a1579
Yurii Zhurakovskyi <yuriizhurakovskyi7@gmail.com>
2019-12-30 19:17:07 +0200

    add homework 8 Events

---
+++ lesson-08-homework/max.js
@@ -0,0 +1,6 @@
+heading.onclick = function() {
+	if (menu.style.display != 'none')
+		menu.style.display = 'none';
+	else
+		menu.style.display = 'block';
+}
---
+++ lesson-08-homework/min.js
@@ -0,0 +1,6 @@
+hideItemButton.onclick = function() {
+	if (hideItemButton.value == 'Hide the button')
+		hideItemButton.style.visibility = "hidden";
+	text.style.visibility = "hidden";
+	hideItemButton.value = 'Hide the button';
+}
